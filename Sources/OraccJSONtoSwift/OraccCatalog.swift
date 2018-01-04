b8c5a063917c49e5b775582e4a887d5353f69709
Chaitanya Kanchan <cdkanchan@gmail.com>
2018-01-04 15:58:53 +0000

    Removed sorting function from OraccCatalog as it was making the Release build crash and it can be recreated in client applications anyway.

--- Sources/OraccJSONtoSwift/OraccCatalog.swift
+++ Sources/OraccJSONtoSwift/OraccCatalog.swift
@@ -11,19 +11,6 @@ public struct OraccCatalog: Decodable {
     public let source: URL
     public let project: String
     public let members: [String: OraccCatalogEntry]
-    
-    public lazy var keys: [String] = {
-        var keys = [String]()
-        for entry in self.members.keys {
-            keys.append(entry)
-        }
-        return keys
-    }()
-    
-    public mutating func sortBySAANum() {
-        let sortedMembers = members.sorted {
-            return $0.value.SAAid < $1.value.SAAid
-        }
-        keys = sortedMembers.map{$0.key}
-    }
 }
+
+
