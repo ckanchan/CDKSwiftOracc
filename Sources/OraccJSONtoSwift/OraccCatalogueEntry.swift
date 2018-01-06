7fe0fc71df1c8419d50527e4eb54e0189442ad32
Chaitanya Kanchan <cdkanchan@gmail.com>
2018-01-06 18:30:14 +0000

    Upgraded the OraccCatalogueEntry struct with new useful fields.

--- Sources/OraccJSONtoSwift/OraccCatalogueEntry.swift
+++ Sources/OraccJSONtoSwift/OraccCatalogueEntry.swift
@@ -8,23 +8,56 @@
 import Foundation
 
 public struct OraccCatalogEntry {
-    public let displayName: String
-    public let title: String
-    public let id: String
-    public let ancientAuthor: String?
-    public let SAAid: Int
     
-    public let chapter: Int
+    /// Short name for referencing
+    public let displayName: String
+    
+    /// Descriptive title for the text content assigned by its editors
+    public let title: String
+    
+    /// CDLI ID, ususally a P or X number
+    public let id: String
+    
+    /// Ancient author, if available
+    public let ancientAuthor: String?
+    
+    /// Path for the originating project
+    public let project: String
+    
+    public let chapterNumber: Int
+    public let chapterName: String
+    public var chapter: String {
+        return "Ch.\(chapterNumber) (\(chapterName))"
+    }
+    public let genre: String?
+    public let material: String?
+    public let period: String?
+    public let provenience: String?
+    public let primaryPublication: String?
+    public let museumNumber: String?
+    public let publicationHistory: String?
+    
+    ///Copyright and editorial information
+    public let credits: String?
 }
 
 extension OraccCatalogEntry: Decodable {
     private enum CodingKeys: String, CodingKey {
         case displayName = "display_name"
-        case title = "title"
+        case title
         case id = "id_text"
         case ancientAuthor = "ancient_author"
+        case project
         
-        case chapter = "ch_no"
+        case chapterNumber = "ch_no"
+        case chapterName = "ch_name"
+        
+        case genre, material, period, provenience
+        case primaryPublication = "primary_publication"
+        case museumNumber = "museum_no"
+        case publicationHistory = "publication_history"
+        case credits
+        
     }
     
     public init(from decoder: Decoder) throws {
@@ -32,14 +65,26 @@ extension OraccCatalogEntry: Decodable {
         let displayName = try container.decode(String.self, forKey: .displayName)
         let title = try container.decode(String.self, forKey: .title)
         let id = try container.decode(String.self, forKey: .id)
-        let chapterStr = try container.decode(String.self, forKey: .chapter)
-        let ancientAuthor = try container.decode(String.self, forKey: .ancientAuthor)
-        let chapter = Int(String(chapterStr.split(separator: " ").last!))!
-        let SAAid = Int(String(displayName.split(separator: " ").last!))!
+        let ancientAuthor = try container.decodeIfPresent(String.self, forKey: .ancientAuthor)
+        let project = try container.decode(String.self, forKey: .project)
+        let chapterNumStr = try container.decode(String.self, forKey: .chapterNumber)
+        let chapterNumber = Int(String(chapterNumStr.split(separator: " ").last!))!
+        let chapterName = try container.decode(String.self, forKey: .chapterName)
+        
+        let genre = try container.decodeIfPresent(String.self, forKey: .genre)
+        let material = try container.decodeIfPresent(String.self, forKey: .material)
+        let period = try container.decodeIfPresent(String.self, forKey: .period)
+        let provenience = try container.decodeIfPresent(String.self, forKey: .provenience)
+        let primaryPublication = try container.decodeIfPresent(String.self, forKey: .primaryPublication)
+        let museumNumber = try container.decodeIfPresent(String.self, forKey: .museumNumber)
+        let publicationHistory = try container.decodeIfPresent(String.self, forKey: .publicationHistory)
+        let credits = try container.decodeIfPresent(String.self, forKey: .credits)
+        
+        self.init(displayName: displayName, title: title, id: id, ancientAuthor: ancientAuthor, project: project, chapterNumber: chapterNumber, chapterName: chapterName, genre: genre, material: material, period: period, provenience: provenience, primaryPublication: primaryPublication, museumNumber: museumNumber, publicationHistory: publicationHistory, credits: credits)
         
         
         
-        self.init(displayName: displayName, title: title, id: id, ancientAuthor: ancientAuthor, SAAid: SAAid, chapter: chapter)
+        
     }
 }
 
