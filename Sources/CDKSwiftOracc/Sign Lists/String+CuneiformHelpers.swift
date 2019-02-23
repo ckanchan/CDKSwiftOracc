3de48a5bcc29bd7e0c0eb361997049c0bc7eb566
Chaitanya Kanchan <cdkanchan@gmail.com>
2019-02-23 12:28:33 +0000

    - `String.parseOSLLine()` now deals with lines that have comments appended to them with [] (by dropping them from output)

--- Sources/CDKSwiftOracc/Sign Lists/String+CuneiformHelpers.swift
+++ Sources/CDKSwiftOracc/Sign Lists/String+CuneiformHelpers.swift
@@ -47,10 +47,18 @@ extension String {
 // OSL routines
 extension String {
     func parseOSLLine() -> (OSL.CommandLine)? {
-        var components = self.split(separator: Character(" "))
+        var components = self.trimOSLComment().components(separatedBy: .whitespaces)
         guard components.count > 0 else {return nil}
         let commandLiteral = components.removeFirst()
-        guard let command = OSL.Command.init(rawValue: String(commandLiteral)) else { return nil}
+        guard let command = OSL.Command(rawValue: String(commandLiteral)) else { return nil }
         return (command, components.map{String($0)})
+    }
+    
+    func trimOSLComment() -> String {
+        if let noteBracketPosition = self.firstIndex(of: "[") {
+            return String(self.prefix(upTo: noteBracketPosition))
+        } else {
+            return self
+        }
     }
 }
